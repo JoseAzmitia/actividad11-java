4ea2e94dfaa888d92969cb08f22668af4333a9f2
JoseAzmitia <azmitiajc@gmail.com>
2021-11-15 01:24:12 -0600

    Creación de interfaz y clases

---
+++ src/Figura.java
@@ -0,0 +1,13 @@
+public class Figura implements Shape{
+
+    Figura (){
+
+    }
+
+    public double getArea(){
+        return 0;
+    }
+    public double getPerimeter(){
+        return 0;
+    }
+}
